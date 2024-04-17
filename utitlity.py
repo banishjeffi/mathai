10a7c0c6041099115507bda4a733143a78212691
banishjeffi <166292800+banishjeffi@users.noreply.github.com>
2024-04-17 18:55:54 +0530

    Delete utitlity.py

--- utitlity.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key = "AIzaSyC6zEe1IMBGSvq5QfOrci-1KLO2Ztkc1Bk")
-
-def load_model():
-    model = genai.GenerativeModel('gemini-pro')
-    return model
